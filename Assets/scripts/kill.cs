45c61157f3ec0da694f57c9ef54bc9e569f0e099
iseeanoob <leander.herstad@gmail.com>
2024-09-25 12:48:28 +0200

    Initial commit

---
+++ Assets/scripts/kill.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class  Killer_cube: MonoBehaviour
+{
+    public int Respawn;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+}
